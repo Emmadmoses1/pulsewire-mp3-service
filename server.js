ed9c614422bb4b07d2afc203440b11b80884fbe9
LTC Mining <ltcmining@website.com>
2026-09-14 04:49:52 +0100

    fix yt-dlp format selection

--- server.js
+++ server.js
@@ -127,7 +127,7 @@ app.post('/convert',async(req,res)=>{
       '--no-playlist',
       '--no-warnings',
       '--restrict-filenames',
-      '-f','bestaudio/best',
+      '-f','bestaudio[ext=webm]/bestaudio[ext=m4a]/bestaudio/best',
       '-o',source,
       url
     ]);
